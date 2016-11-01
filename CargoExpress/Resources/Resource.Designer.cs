f83d44da4658486fdea7940f8033bd396fda582e
Pablo Andrés Pérez Quijada <pablo.perez.quijada@outlook.com>
2016-11-01 00:48:10 -0300

    add project

--- CargoExpress/Resources/Resource.Designer.cs
+++ CargoExpress/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace CargoExpress
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int cargoExpress = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -60,8 +63,41 @@ namespace CargoExpress
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050009
+			public const int btnLogin = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int btnLogout = 2131034122;
+			
+			// aapt resource value: 0x7f050005
+			public const int checkBox1 = 2131034117;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int imageView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int lblPass = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int lblUser = 2131034113;
+			
+			// aapt resource value: 0x7f05000b
+			public const int progressBar1 = 2131034123;
+			
+			// aapt resource value: 0x7f050006
+			public const int tableLayout1 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int tableRow1 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView2 = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int txtPassword = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtUserName = 2131034114;
 			
 			static Id()
 			{
@@ -92,11 +128,14 @@ namespace CargoExpress
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int ApplicationName = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int Exit = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int Login = 2130968576;
 			
 			static String()
 			{
